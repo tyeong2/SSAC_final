5b9773b212d14a26069e85ca38ceb4586353a8ca
ymkim <77034505+ymkim04158@users.noreply.github.com>
2021-04-24 20:38:31 +0900

    django_social_share 추가

--- community/settings.py
+++ community/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'board.apps.BoardConfig'
+    'board.apps.BoardConfig',
+    'django_social_share'
 ]
 
 MIDDLEWARE = [
