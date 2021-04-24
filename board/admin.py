e4f517b8e3fcc9d09c9385f984189204a9e9f793
tyeong2 <31345464+tyeong2@users.noreply.github.com>
2021-04-24 19:29:27 +0900

    칼럼명 수정 반영, 사용자 저장 이미지 admin 추가

--- board/admin.py
+++ board/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Member, Board
+from .models import Member, Board, UserCar
 
 # Register your models here.
 
@@ -7,7 +7,11 @@ class MemberAdmin(admin.ModelAdmin):
     list_display = ('user_email', 'user_nickname', 'user_pw', 'user_created', 'user_updated')
 
 class BoardAdmin(admin.ModelAdmin):
-    list_display = ('title', 'writer', 'created_at', 'updated_at', 'image', 'hit')
+    list_display = ('title', 'writer', 'created_at', 'updated_at', 'image_path', 'hit')
+
+class UserCarAdmin(admin.ModelAdmin):
+    list_display = ('member', 'type', 'brand', 'img_path', 'saved_at')
 
 admin.site.register(Member, MemberAdmin)
 admin.site.register(Board, BoardAdmin)
+admin.site.register(UserCar, UserCarAdmin)
